05a68ad640c9842b66868469450525861da29010
Jeff <jeff999955@gmail.com>
2022-07-05 10:49:57 +0800

    Add redis cache

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const { Pool } = require("pg");
+const { createClient } = require("redis");
 require("dotenv").config();
+
 const config = {
   host: process.env.PSQL_HOSTNAME,
   user: process.env.PSQL_USERNAME,
   password: process.env.PSQL_PASSWD,
   database: process.env.PSQL_DB,
-  port: process.env.PSQL_PORT,
+  port: 5432,
   ssl: true,
 };
 
@@ -14,6 +16,23 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const pool = new Pool(config);
+
+let redisConnected = false;
+const cacheHostName = process.env.REDISCACHEHOSTNAME;
+const cachePassword = process.env.REDISCACHEKEY;
+const cacheConnection = createClient({
+  url: "rediss://" + cacheHostName + ":6380",
+  password: cachePassword,
+});
+const setupRedis = async () => {
+  if (!redisConnected) {
+    await cacheConnection.connect();
+    redisConnected = true;
+  }
+  return cacheConnection;
+};
+
+let isDirty = true;
 
 app.get("/", (req, res) => {
   res
@@ -23,22 +42,39 @@ app.get("/", (req, res) => {
     .end();
 });
 
-app.get("/hist_log", (req, res) => {
-  pool
-    .query(
+const getHistory = async () => {
+  try {
+    const queryResult = await pool.query(
       `SELECT * FROM azure_table \
         ORDER BY timestamp DESC \
         LIMIT 5
         `
-    )
-    .then((queryResult) => {
-      let responseString = queryResult["rows"]
-        .map((row) => `${row.num1} + ${row.num2} = ${row.sum}`)
-        .join("\n");
-      console.log(responseString);
-      res.end(responseString);
-    })
-    .catch((err) => console.error(err));
+    );
+
+    let responseString = queryResult["rows"]
+      .map((row) => `${row.num1} + ${row.num2} = ${row.sum}`)
+      .join("\n");
+    console.log(responseString);
+    return responseString;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+app.get("/hist_log", async (req, res) => {
+  const client = await setupRedis();
+  const cachedValue = await client.get("cache");
+  if (isDirty || !cachedValue) {
+    const responseString = await getHistory();
+    if (responseString === undefined) res.sendStatus(400);
+    await client.set("cache", responseString);
+    console.log("from source data");
+    res.send(responseString);
+    isDirty = false;
+  } else {
+    console.log("from cached data");
+    res.send(cachedValue);
+  }
 });
 
 app.post("/calculator", (req, res) => {
@@ -56,13 +92,16 @@ app.post("/calculator", (req, res) => {
       )
       .then((res) => console.log(res))
       .catch((err) => console.error(err));
+    isDirty = true;
   } catch (e) {
-    res.sendStatus(400);
+    console.error(e);
   }
 });
 
 app.listen(port, async () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+
+  setupRedis().then(() => console.log("Redis connection set!"));
   pool
     .query(
       "CREATE TABLE IF NOT EXISTS azure_table \
@@ -74,11 +113,9 @@ app.listen(port, async () => {
         )"
     )
     .then(() => console.log("Table created!"));
-    
-    pool
-    .query(
-      `SELECT * FROM azure_table`
-    )
+
+  pool
+    .query(`SELECT * FROM azure_table`)
     .then((queryResult) => {
       let responseString = queryResult["rows"]
         .map((row) => `${row.num1} + ${row.num2} = ${row.sum}`)
